4de7da2edb5e1f926b6082eed32b5997dc9591a8
ayeshatabassum1406 <64879625+ayeshatabassum1406@users.noreply.github.com>
2021-02-28 17:40:56 +0530

    Create program7.php

---
+++ program7/program7.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+<title>program 7</title>
+<meta http-equiv="refresh" content="1"/>
+<style>
+    p{
+        color:white;
+        font-size:90px;
+        position:absolute;
+        top:50%;
+        left:50%;
+        transform:translate(-50%,-50%);
+          background-color:black;
+    }
+    body{
+        background-color:red;
+
+    }
+</style>
+   
+
+
+     <p><?php
+			date_default_timezone_set('Asia/Calcutta');
+
+            echo date("h :i:s A");
+        ?>
+    </p>
+</head>
+
+</html>
+</html>
